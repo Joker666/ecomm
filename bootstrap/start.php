0da593c18f90acb755ebe311248174869f33afbc
joker666 <mr.k779@outlook.com>
2014-02-07 03:29:39 +0600

    ggg

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,17 +24,17 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-/*$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(array(
 
 	//'local' => array('your-machine-name'),
-    'development' => array('Joker')
+    'development' => array('')
 
-));*/
-$env = $app->detectEnvironment(function () {
+));
+/*$env = $app->detectEnvironment(function () {
     return isset($_SERVER['LARAVEL_ENV'])
         ? $_SERVER['LARAVEL_ENV']
         : 'development'; // or whatever fallback you prefer
-});
+});*/
 
 /*
 |--------------------------------------------------------------------------
